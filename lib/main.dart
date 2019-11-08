e6f1c12526d1c108d0932a3b021301f26da6b5b9
aki12n <nakanoaki12@gmail.com>
2019-11-09 00:10:00 +0900

    複数の写真が選べる様にライブラリ変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,99 @@
-import 'dart:io';
-import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
-import 'package:simple_permissions/simple_permissions.dart';
+import 'dart:async';
+
 import 'package:multi_image_picker/multi_image_picker.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Generated App',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: ImagePage(),
-      themeMode: ThemeMode.dark,
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Asset> images = List<Asset>();
+  String _error;
+  String _createState = '';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget buildGridView() {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: List.generate(images.length, (index) {
+        Asset asset = images[index];
+        return AssetThumb(
+          asset: asset,
+          width: 300,
+          height: 200,
+        );
+      }),
     );
   }
-}
 
-class ImagePage extends StatefulWidget {
-  @override
-  _ImagePageState createState() => _ImagePageState();
-}
-
-class _ImagePageState extends State<ImagePage> {
-  File image;
-  List _images;
-  double _r;
-  double _g;
-  double _b;
-
-  _MyImagePageState() {
-    requestPermission();
+  Widget buildKumiView() {
+    return ButtonBar();
   }
 
-  void requestPermission() async {
-    await SimplePermissions.requestPermission(Permission.Camera);
-    if (Platform.isAndroid &&
-        !await SimplePermissions.checkPermission(
-            Permission.WriteExternalStorage)) {
-      await SimplePermissions.requestPermission(
-          Permission.WriteExternalStorage);
-    } else if (Platform.isIOS &&
-        !await SimplePermissions.checkPermission(Permission.PhotoLibrary)) {
-      await SimplePermissions.requestPermission(Permission.PhotoLibrary);
+  void createKumic() {
+    _createState = 'tukuruyo';
+  }
+
+  Future<void> loadAssets() async {
+    setState(() {
+      images = List<Asset>();
+    });
+
+    List<Asset> resultList;
+    String error;
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 300,
+      );
+    } on Exception catch (e) {
+      error = 'error';
     }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      images = resultList;
+      if (error == null) _error = 'No Error Dectected';
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Hoge!'),
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Plugin example app'),
         ),
-        body: Listener(
-          child: Container(
-            child: CustomPaint(
-              child: ConstrainedBox(
-                constraints: BoxConstraints.expand(),
-              ),
+        body: Column(
+          children: <Widget>[
+            Center(child: Text('Error: $_error')),
+            RaisedButton(
+              child: Text("Pick images"),
+              onPressed: loadAssets,
             ),
-          ),
+            Expanded(
+              child: buildGridView(),
+            ),
+            RaisedButton(
+              child: Text("Create Kumic!"),
+              onPressed: createKumic,
+            ),
+            Center(child: Text(_createState)),
+          ],
         ),
-        floatingActionButton: image == null
-            ? FloatingActionButton(
-                onPressed: _getImages,
-                tooltip: 'select pictures!',
-                child: Icon(Icons.photo_album),
-              )
-            : FloatingActionButton(
-                onPressed: saveImage,
-                tooltip: 'save Image!',
-                child: Icon(Icons.save),
-              ));
-  }
-
-  void _getImages() async {
-    _images = await MultiImagePicker.pickImages(
-      maxImages: 10,
-      enableCamera: true,
+      ),
     );
-    image = _images.first;
   }
-
-  void saveImage() {}
 }
